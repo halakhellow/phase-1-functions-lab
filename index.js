fd3b6968a46f9944c3f4a13a1bcae543945e3a20
Hala Alkhellow <halakhellow1996@gmail.com>
2022-04-11 17:45:46 +0300

    add calculatesFarePrice function defining multiple cases

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function distanceTravelledInFeet(start,destination){
     let distance = (destination-start)*264
     return distance<0 ? -distance : distance
 }
+
+function calculatesFarePrice(start,destination){
+    let distance = distanceTravelledInFeet(start,destination);
+    if(distance <= 400) return 0
+    else if (distance >400 && distance<=2000) return (distance-400)*0.02
+    else if (distance > 2000 && distance <= 2500) return 25
+    else return 'cannot travel that far'
+}
