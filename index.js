93fa1f028f02b41e9698675e617c673741511e5f
Hala Alkhellow <halakhellow1996@gmail.com>
2022-04-11 17:44:39 +0300

    add distanceFromHqInFeet function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function distanceFromHqInBlocks(passengerLocation){
     return Math.max(passengerLocation,42)-Math.min(passengerLocation,42)
 }
+
+function distanceFromHqInFeet(passengerLocation){
+    return distanceFromHqInBlocks(passengerLocation)*264
+}
